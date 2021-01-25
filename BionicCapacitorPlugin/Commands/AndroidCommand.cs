caab36ae01413fa3f093e5afd342096cd6b86845
sabitertan <sabitertan@gmail.com>
2021-01-25 18:08:27 -0500

    Updated commands for new CommanLineUtils patterns

--- BionicCapacitorPlugin/Commands/AndroidCommand.cs
+++ BionicCapacitorPlugin/Commands/AndroidCommand.cs
@@ -3,10 +3,10 @@ using McMaster.Extensions.CommandLineUtils;
 
 namespace BionicCapacitorPlugin.Commands {
   [Command(Name = "android", Description = "Capacitor Android commands")]
-  [Subcommand("build", typeof(AndroidBuildCommand))]
-  [Subcommand("init", typeof(AndroidInitCommand))]
-  [Subcommand("open", typeof(AndroidOpenCommand))]
-  [Subcommand("sync", typeof(AndroidSyncCommand))]
+  [Subcommand(typeof(AndroidBuildCommand))]
+  [Subcommand(typeof(AndroidInitCommand))]
+  [Subcommand(typeof(AndroidOpenCommand))]
+  [Subcommand(typeof(AndroidSyncCommand))]
   public class AndroidCommand : CommandBase {
     protected override int OnExecute(CommandLineApplication app) => Init(app);
 
--- BionicCapacitorPlugin/Commands/IOSCommand.cs
+++ BionicCapacitorPlugin/Commands/IOSCommand.cs
@@ -3,10 +3,10 @@ using McMaster.Extensions.CommandLineUtils;
 
 namespace BionicCapacitorPlugin.Commands {
   [Command(Name = "ios", Description = "Capacitor iOS commands")]
-  [Subcommand("build", typeof(IOSBuildCommand))]
-  [Subcommand("init", typeof(IOSInitCommand))]
-  [Subcommand("open", typeof(IOSOpenCommand))]
-  [Subcommand("sync", typeof(IOSSyncCommand))]
+  [Subcommand(typeof(IOSBuildCommand))]
+  [Subcommand(typeof(IOSInitCommand))]
+  [Subcommand(typeof(IOSOpenCommand))]
+  [Subcommand(typeof(IOSSyncCommand))]
   public class IOSCommand : CommandBase {
     protected override int OnExecute(CommandLineApplication app) => Init(app);
 
